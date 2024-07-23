f0674c9136fe4adc9b09a64a49f7c63763fc396e
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-23 23:55:41 +0530

    Adding the basic data

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
-import HomeMotion from "@/components/BasicsOfMotion";
-import Gestures from "@/components/Gestures";
 
 export default function Home() {
   return (
-    <Gestures />
-  )
+    <main>
+      <div></div>
+    </main>
+  );
 }
