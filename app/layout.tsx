969237940bfcc1440296b99049e9659dd698face
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-24 23:26:35 +0530

    Adding navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
+import { BsPlusCircle } from "react-icons/bs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,45 +21,46 @@ export default function RootLayout({
       <body
         className={`inter.className bg-[#f0f0f0] flex flex-col gap-4 items-center justify-center my-4`}
       >
-        <header className="w-[560px] p-4 bg-white rounded-md">
-          <nav className="flex justify-between">
-            <div className="flex gap-4">
-              <div>
+        <header className="w-[560px] h-[64px] bg-white rounded-xl flex items-center p-4">
+          <nav className="flex w-full justify-between">
+            <div className="flex gap-8 p-2 relative">
+              <div className="w-[38px] h-[38px] bg-[#f0f0f0] absolute rounded-full opacity-70 top-[1px] left-[0.5px] "></div>
+              <div className="z-10">
                 <Image
                   src="/navbar/home.png"
                   alt="home"
-                  width={25}
-                  height={25}
+                  width={22}
+                  height={22}
                 />
               </div>
-              <div>
+              <div className="z-10">
                 <Image
                   src="/navbar/about.png"
                   alt="home"
-                  width={25}
-                  height={25}
+                  width={23}
+                  height={23}
                 />
               </div>
-              <div>
+              <div className="z-10">
                 <Image
                   src="/navbar/projects.png"
                   alt="home"
-                  width={25}
-                  height={25}
+                  width={23}
+                  height={23}
                 />
               </div>
-              <div>
+              <div className="z-10">
                 <Image
                   src="/navbar/products.png"
                   alt="home"
                   width={25}
-                  height={25}
+                  height={24}
                 />
               </div>
             </div>
             <div>
-              <div className="bg-black w-[92.16pxz] flex gap-2 rounded-full">
-                <span className="text-white border-2 border-white rounded-full">+</span>
+              <div className="bg-black w-[92.16pxz] flex gap-2 rounded-lg box-border p-2 cursor-pointer items-center font-semibold">
+                <BsPlusCircle className="text-white" />
                 <span className="text-white font-semibold">Hire Me</span>
               </div>
             </div>
